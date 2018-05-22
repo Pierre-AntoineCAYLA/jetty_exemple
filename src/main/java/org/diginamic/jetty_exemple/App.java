d539e90349afdd737d5245194585268add84df49
Pierre-Antoine <you@example.com>
2018-05-22 11:25:20 +0200

    mon premier jetty

---
+++ src/main/java/org/diginamic/jetty_exemple/App.java
@@ -0,0 +1,12 @@
+package org.diginamic.jetty_exemple;
+
+public class App {
+
+	/**
+	 * Hello world!
+	 *
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
